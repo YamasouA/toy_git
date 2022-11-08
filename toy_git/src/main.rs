9cb9e3ca96882243c06685825210a4d95b91d47f
YamasouA <akiakiskyhand@gmail.com>
2022-11-08 12:24:23 +0900

    commit object

--- toy_git/src/main.rs
+++ toy_git/src/main.rs
@@ -96,7 +96,136 @@ impl Tree {
 	}
 }
 
+pub struct User {
+	pub name: String,
+	pub email: String,
+	pub ts: DateTime<FixedOffset>,
+}
 
+pub struct Commit {
+	pub tree: String,
+	pub parent: Option<String>, // 最初のコミットにはparentが存在しないからOption
+	pub author: User,
+	pub committer: User,
+	pub message: String,
+}
+
+impl Commit {
+	pub fn from(bytes: &[u8]) -> Option<Self> {
+		// commitメッセージとの間に空行があるからfilterにかける
+		let mut iter = bytes.split(|&x| x == b'\n').filter(|x| x != b"");
+
+		let tree = iter
+			.next()
+			.map(|x| {
+				x.splitn(2, |&x| x == b' ')
+					.skip(1) // 最初の要素はtreeで決まっているからスキップする
+					.flatten()
+					.map(|&x| x)
+					.collect::<Vec<_>>()
+			})
+			.and_then(|x| String::from_utf8(x).ok())?;
+
+		let parent = &iter
+			.next()
+			.map(|x| {
+				x.splitn(2, |&x| x == b' ')
+					.map(Vec::from)
+					.map(|x| String::from_utf8(x).ok().unwrap_or_default())
+					.collect::<Vec<_>>()
+			})
+			.ok_or(Vec::new())
+			.and_then(|x| match x[0].as_str() {
+				"parent" => Ok(x[1].clone()), // 最初の文字列がparentなら
+				_ => Err(|[x[0]].as_bytes(), b" ", x[1].asbytes()].concat()), // そうでなければ元の形に戻してErrに包む
+			});
+
+		let author = match parent {
+			Ok(_) => iter.next().map(|x| Vec::from(x)), // parentがOkならiteratorからとる
+			Err(v) => Some(v.clone()), // Errならその値を使う
+		}
+		.map(|x| {
+			x.splitn(2, |&x| x == b' ')
+				.skip(1)
+				.flatten()
+				.map(|&x| x)
+				.collect::<Vec<_>>()
+		})
+		.and_then(|x| User::from(x.as_slice()))?;
+
+		let commiter = iter
+			.next()
+			.map(|x| {
+				x.splitn(2, |&x| x == b' ')
+					.skip(1)
+					.flatten()
+					.map(|&x| x)
+					.collect::<Vec<_>>()
+			})
+			.and_then(|x| User::from(x.as_slice()))?;
+
+		let message = iter
+			.next()
+			.map(Vec::from)
+			.and_then(|x| String::from_utf8(x).ok())?;
+
+		Some(Self::new(
+			tree,
+			parent.clone().ok(),
+			author,
+			committer,
+			message,
+		))
+	}
+}
+
+impl User{
+	pub fn from(bytes: &[u8]) -> Option<Self> {
+		let name = String::from_utf8(
+			bytes
+				.into_iter()
+				.take_while(|&&x| x != b'<')
+				.map(|&x| x)
+				.collect(),
+		}
+		.map(|x| String::from(x.trim())) // 最後の空白をtrimする
+		.ok()?;
+
+		let info = String::from_utf8(
+			bytes
+				.into_iter()
+				.skip_while(|&&x| x != b'<') // 関数がtrueの間要素を捨てる
+				.map(|&x| x)
+				.collect(),
+		)
+		.ok()?;
+
+		let mut info_iter = info.splitn(3, " ");
+
+		let email = info_iter
+			.next()
+			.map(|x| String::from(x.trim_matches(|x| x == '<' || x == '>')))?;
+
+		// and_then return None if option is None, otherwise calls f
+		let ts = Utc.timestamp(infow_iter.next().and_then(|x| x.parse::<i64>().ok())?, 0);
+		let offset = info_iter
+			.next()
+			.and_then(|x| x.parse::<i32>().ok())
+			.map(|x| {
+				if x < 0 {
+					FixedOffset::west(x / 100 * 60 * 60)
+				} else {
+					FixedOffset::east(x / 100 * 60 * 60)
+				}
+			})?;
+
+		Some(Self::new(
+			name,
+			email,
+			offset.from_utc_datetime(&ts.naive_utc()), // UTC時間のタイムスタンプにoffsetをつける
+		))
+	}
+}
 
 fn main() {
     println!("Hello, world!");
